81d017997c097659a00f75c5a643c2106245fa92
AhmedAlsohail <ahmadalsohail@gmail.com>
2025-05-19 06:37:20 +0300

    Declaring a Vector Type

---
+++ Project/headers/vector.h
@@ -0,0 +1,18 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct{
+    float x;
+    float y;
+} vector2;
+
+typedef struct{
+    float x;
+    float y;
+    float z;
+} vector3;
+
+
+// TODO: Add vectors manipulation function
+
+#endif
